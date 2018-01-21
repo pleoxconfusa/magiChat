0347c4e93f0b48114e8311317c4efe91d7e0806f
conorjamesrogers <conorjames.rogers@gmail.com>
2018-01-20 22:41:47 -0800

    server and client use simple

--- simple_server.py
+++ simple_server.py
@@ -1,8 +1,28 @@
 import socket
+
+def load_encoding():
+    #Function to load dictionary of ascii -> 48bit data from text file
+    scheme_dict = {}
+    #load file
+    #[########]:HEX
+    with open(magic_file_name, 'r') as f:
+        for lines in f:
+            x = lines.split('[')[1]
+            scheme_dict[x.split(']')[0]] = lines.split(':')[1].replace('\n','')
+        f.close()
+
+    #generate dictionary from file
+    printf(scheme_dict)
+    #return dictionary
+    #48 bit -bitstream
+    #12 length hex per char
+    return scheme_dict
  
 def Main():
     host = "127.0.0.1"
-    port = 5000
+    port = 63544
+    scheme_dict = load_encoding()
+
      
     mySocket = socket.socket()
     mySocket.bind((host,port))
