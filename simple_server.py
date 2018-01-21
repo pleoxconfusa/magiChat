92cfbdf1236ec9719102bf136082be96fc09f1cb
conorjamesrogers <conorjames.rogers@gmail.com>
2018-01-20 23:38:26 -0800

    fixed encoding

--- simple_server.py
+++ simple_server.py
@@ -1,6 +1,7 @@
 import socket
 
 magic_file_name = "squares.txt"
+BUFFER = 20480
 
 def load_encoding():
     #Function to load dictionary of ascii -> 48bit data from text file
@@ -10,19 +11,22 @@ def load_encoding():
     with open(magic_file_name, 'r') as f:
         for lines in f:
             x = lines.split(':')[0]
-            scheme_dict[x] = chr(int(lines.split(':')[1].replace('\n','')))
+            x_list = x.split(' ')
+            x_list = x_list[:-1]
+            x_list = list(map(int, x_list))
+            x_tuple = tuple(x_list)
+            scheme_dict[x_tuple] = chr(int(lines.split(':')[1].replace('\n','')))
         f.close()
 
     #generate dictionary from file
-    # print(scheme_dict)
+    print(scheme_dict)
     #return dictionary
-    #48 bit -bitstream
-    #12 length hex per char
+
     return scheme_dict
  
-def decode(message, scheme_dict):
+# def decode(message, scheme_dict):
     #take in the message and pull out magic squares
-    x = list(message)
+    #80 bits into 16 -> 5 bit bitstrings
 
     #if magic square matches, keep it, else store it.
     #reconstruct magic squares that need to be reconstructed
@@ -40,7 +44,7 @@ def Main():
     conn, addr = mySocket.accept()
     print ("Connection from: " + str(addr))
     while True:
-            data = conn.recv(1024).decode()
+            data = conn.recv(BUFFER).decode()
             if not data:
                     break
             print ("from connected  user: " + str(data))
